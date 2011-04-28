8043cf96edfbc59c5947374d9a1ded0cc53d3312
Ryan Stawarz <ryan@stawarz.com>
2011-04-27 22:35:20 -0500

    Beginning to load models

--- lib/vain_models/engine.rb
+++ lib/vain_models/engine.rb
@@ -6,5 +6,23 @@ module VainModels
 		end
 	end
 	
-
+	class Manifest
+	  attr_accessor :models
+	  def initialize
+	    find_models
+	  end
+	  
+	  def model_dir
+	    Dir.join(Rails.root, 'app', 'models')
+	  end
+	  
+	  def find_model_files
+      model_files = []
+        Dir.chdir(model_dir) do
+          model_files = Dir["**/*.rb"]
+        end
+      
+      model_files
+    end
+  end
 end
